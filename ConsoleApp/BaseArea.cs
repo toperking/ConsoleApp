7a26da43dfa494dc74e2048c3e5c737412114e04
Tony Chen <tony.chen@vst.co.jp>
2026-03-05 15:38:07 +0800

    變更Merge branch 'console02' of https://github.com/toperking/ConsoleApp into console02

--- ConsoleApp/BaseArea.cs
+++ ConsoleApp/BaseArea.cs
@@ -92,6 +92,24 @@ namespace ConsoleApp
         }
     }
 
+    public class TESTdArea : IBaseArea
+    {
+        public float top;
+        public float button;
+        public float height;
+        public TESTdArea(float top, float button, float height)
+        {
+            this.top = top;
+            this.button = button;
+            this.height = height;
+        }
+        public void Area()
+        {
+            var area = (top + button) * height / 2;
+            Console.WriteLine($"一般面積是:{area}");
+        }
+    }
+
     public class TestArea : IBaseArea
     {
         public float top;
