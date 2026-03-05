641f620b47cf74cd933035d202a26d0c4488487b
Tony Chen <tony.chen@vst.co.jp>
2026-03-05 14:08:57 +0800

    建立Create
    建立

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+namespace ConsoleApp
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("計算面積開始:");
+            new AreaFactory(new CircleArea(2)).Area();
+            new AreaFactory(new RectangleArea(2,3)).Area();
+            new AreaFactory(new TriangleArea(2,3)).Area();
+            new AreaFactory(new TrapezoidArea(2, 3,2)).Area();
+        }
+    }
+}
