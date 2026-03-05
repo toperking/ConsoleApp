70ecc28fbc7de368f1e38721abd0fcb266cdfb98
Tony Chen <tony.chen@vst.co.jp>
2026-03-05 15:22:18 +0800

    變更

--- ConsoleApp/obj/Debug/net10.0/ConsoleApp.AssemblyInfo.cs
+++ ConsoleApp/obj/Debug/net10.0/ConsoleApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cde00a5ef6b8c341475fe1bb0b9e772aac232e4a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0671de2c6ce4e8be0862c5b1beda120672573754")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
